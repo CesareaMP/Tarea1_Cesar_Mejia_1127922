3736033bdc4865a2399932f5e3951b3b476990dd
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2022-09-12 13:53:27 -0600

    Finalización de ejercicio 1 forma recursiva e iterativa

--- Tarea1_Cesar_Mejia_1127922/MyForm.h
+++ Tarea1_Cesar_Mejia_1127922/MyForm.h
@@ -8,6 +8,7 @@ namespace Tarea1CesarMejia1127922 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Resumen de MyForm
@@ -45,6 +46,19 @@ namespace Tarea1CesarMejia1127922 {
 	private: System::Windows::Forms::Label^ lblmulticando;
 	private: System::Windows::Forms::TabPage^ PALABRAS;
 	private: System::Windows::Forms::TabPage^ BASES;
+	private: System::Windows::Forms::Label^ ITERATIVO;
+	private: System::Windows::Forms::Label^ itersuma;
+
+	private: System::Windows::Forms::Button^ bttniter;
+
+	private: System::Windows::Forms::TextBox^ txtitecador;
+
+	private: System::Windows::Forms::TextBox^ txtitecando;
+
+	private: System::Windows::Forms::Label^ lblitecador;
+
+	private: System::Windows::Forms::Label^ lblitecando;
+	private: System::Windows::Forms::Label^ RECURSIVO;
 
 	private:
 		/// <summary>
@@ -61,14 +75,22 @@ namespace Tarea1CesarMejia1127922 {
 		{
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->SUMAS = (gcnew System::Windows::Forms::TabPage());
+			this->ressumas = (gcnew System::Windows::Forms::Label());
+			this->bttsumas = (gcnew System::Windows::Forms::Button());
+			this->txtmulticador = (gcnew System::Windows::Forms::TextBox());
+			this->txtmulticando = (gcnew System::Windows::Forms::TextBox());
+			this->lblmulticador = (gcnew System::Windows::Forms::Label());
+			this->lblmulticando = (gcnew System::Windows::Forms::Label());
 			this->PALABRAS = (gcnew System::Windows::Forms::TabPage());
 			this->BASES = (gcnew System::Windows::Forms::TabPage());
-			this->lblmulticando = (gcnew System::Windows::Forms::Label());
-			this->lblmulticador = (gcnew System::Windows::Forms::Label());
-			this->txtmulticando = (gcnew System::Windows::Forms::TextBox());
-			this->txtmulticador = (gcnew System::Windows::Forms::TextBox());
-			this->bttsumas = (gcnew System::Windows::Forms::Button());
-			this->ressumas = (gcnew System::Windows::Forms::Label());
+			this->ITERATIVO = (gcnew System::Windows::Forms::Label());
+			this->RECURSIVO = (gcnew System::Windows::Forms::Label());
+			this->itersuma = (gcnew System::Windows::Forms::Label());
+			this->bttniter = (gcnew System::Windows::Forms::Button());
+			this->txtitecador = (gcnew System::Windows::Forms::TextBox());
+			this->txtitecando = (gcnew System::Windows::Forms::TextBox());
+			this->lblitecador = (gcnew System::Windows::Forms::Label());
+			this->lblitecando = (gcnew System::Windows::Forms::Label());
 			this->tabControl1->SuspendLayout();
 			this->SUMAS->SuspendLayout();
 			this->SuspendLayout();
@@ -83,11 +105,19 @@ namespace Tarea1CesarMejia1127922 {
 			this->tabControl1->Multiline = true;
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(519, 288);
+			this->tabControl1->Size = System::Drawing::Size(519, 565);
 			this->tabControl1->TabIndex = 0;
 			// 
 			// SUMAS
 			// 
+			this->SUMAS->Controls->Add(this->itersuma);
+			this->SUMAS->Controls->Add(this->bttniter);
+			this->SUMAS->Controls->Add(this->txtitecador);
+			this->SUMAS->Controls->Add(this->txtitecando);
+			this->SUMAS->Controls->Add(this->lblitecador);
+			this->SUMAS->Controls->Add(this->lblitecando);
+			this->SUMAS->Controls->Add(this->RECURSIVO);
+			this->SUMAS->Controls->Add(this->ITERATIVO);
 			this->SUMAS->Controls->Add(this->ressumas);
 			this->SUMAS->Controls->Add(this->bttsumas);
 			this->SUMAS->Controls->Add(this->txtmulticador);
@@ -97,10 +127,61 @@ namespace Tarea1CesarMejia1127922 {
 			this->SUMAS->Location = System::Drawing::Point(4, 25);
 			this->SUMAS->Name = L"SUMAS";
 			this->SUMAS->Padding = System::Windows::Forms::Padding(3);
-			this->SUMAS->Size = System::Drawing::Size(511, 259);
+			this->SUMAS->Size = System::Drawing::Size(511, 536);
 			this->SUMAS->TabIndex = 0;
 			this->SUMAS->Text = L"SUMAS";
 			this->SUMAS->UseVisualStyleBackColor = true;
+			this->SUMAS->Click += gcnew System::EventHandler(this, &MyForm::SUMAS_Click);
+			// 
+			// ressumas
+			// 
+			this->ressumas->AutoSize = true;
+			this->ressumas->Location = System::Drawing::Point(24, 198);
+			this->ressumas->Name = L"ressumas";
+			this->ressumas->Size = System::Drawing::Size(0, 16);
+			this->ressumas->TabIndex = 5;
+			// 
+			// bttsumas
+			// 
+			this->bttsumas->Location = System::Drawing::Point(330, 61);
+			this->bttsumas->Name = L"bttsumas";
+			this->bttsumas->Size = System::Drawing::Size(148, 78);
+			this->bttsumas->TabIndex = 4;
+			this->bttsumas->Text = L"Calcular";
+			this->bttsumas->UseVisualStyleBackColor = true;
+			this->bttsumas->Click += gcnew System::EventHandler(this, &MyForm::bttsumas_Click);
+			// 
+			// txtmulticador
+			// 
+			this->txtmulticador->Location = System::Drawing::Point(194, 126);
+			this->txtmulticador->Name = L"txtmulticador";
+			this->txtmulticador->Size = System::Drawing::Size(100, 22);
+			this->txtmulticador->TabIndex = 3;
+			// 
+			// txtmulticando
+			// 
+			this->txtmulticando->Location = System::Drawing::Point(194, 61);
+			this->txtmulticando->Name = L"txtmulticando";
+			this->txtmulticando->Size = System::Drawing::Size(100, 22);
+			this->txtmulticando->TabIndex = 2;
+			// 
+			// lblmulticador
+			// 
+			this->lblmulticador->AutoSize = true;
+			this->lblmulticador->Location = System::Drawing::Point(24, 129);
+			this->lblmulticador->Name = L"lblmulticador";
+			this->lblmulticador->Size = System::Drawing::Size(145, 16);
+			this->lblmulticador->TabIndex = 1;
+			this->lblmulticador->Text = L"Ingrese el multiplicador";
+			// 
+			// lblmulticando
+			// 
+			this->lblmulticando->AutoSize = true;
+			this->lblmulticando->Location = System::Drawing::Point(24, 64);
+			this->lblmulticando->Name = L"lblmulticando";
+			this->lblmulticando->Size = System::Drawing::Size(148, 16);
+			this->lblmulticando->TabIndex = 0;
+			this->lblmulticando->Text = L"Ingrese el multiplicando";
 			// 
 			// PALABRAS
 			// 
@@ -121,60 +202,79 @@ namespace Tarea1CesarMejia1127922 {
 			this->BASES->Text = L"BASES";
 			this->BASES->UseVisualStyleBackColor = true;
 			// 
-			// lblmulticando
+			// ITERATIVO
 			// 
-			this->lblmulticando->AutoSize = true;
-			this->lblmulticando->Location = System::Drawing::Point(29, 31);
-			this->lblmulticando->Name = L"lblmulticando";
-			this->lblmulticando->Size = System::Drawing::Size(148, 16);
-			this->lblmulticando->TabIndex = 0;
-			this->lblmulticando->Text = L"Ingrese el multiplicando";
+			this->ITERATIVO->AutoSize = true;
+			this->ITERATIVO->Location = System::Drawing::Point(24, 259);
+			this->ITERATIVO->Name = L"ITERATIVO";
+			this->ITERATIVO->Size = System::Drawing::Size(78, 16);
+			this->ITERATIVO->TabIndex = 6;
+			this->ITERATIVO->Text = L"ITERATIVO";
 			// 
-			// lblmulticador
+			// RECURSIVO
 			// 
-			this->lblmulticador->AutoSize = true;
-			this->lblmulticador->Location = System::Drawing::Point(29, 96);
-			this->lblmulticador->Name = L"lblmulticador";
-			this->lblmulticador->Size = System::Drawing::Size(145, 16);
-			this->lblmulticador->TabIndex = 1;
-			this->lblmulticador->Text = L"Ingrese el multiplicador";
+			this->RECURSIVO->AutoSize = true;
+			this->RECURSIVO->Location = System::Drawing::Point(24, 22);
+			this->RECURSIVO->Name = L"RECURSIVO";
+			this->RECURSIVO->Size = System::Drawing::Size(86, 16);
+			this->RECURSIVO->TabIndex = 7;
+			this->RECURSIVO->Text = L"RECURSIVO";
 			// 
-			// txtmulticando
+			// itersuma
 			// 
-			this->txtmulticando->Location = System::Drawing::Point(199, 28);
-			this->txtmulticando->Name = L"txtmulticando";
-			this->txtmulticando->Size = System::Drawing::Size(100, 22);
-			this->txtmulticando->TabIndex = 2;
+			this->itersuma->AutoSize = true;
+			this->itersuma->Location = System::Drawing::Point(24, 446);
+			this->itersuma->Name = L"itersuma";
+			this->itersuma->Size = System::Drawing::Size(0, 16);
+			this->itersuma->TabIndex = 13;
 			// 
-			// txtmulticador
+			// bttniter
 			// 
-			this->txtmulticador->Location = System::Drawing::Point(199, 93);
-			this->txtmulticador->Name = L"txtmulticador";
-			this->txtmulticador->Size = System::Drawing::Size(100, 22);
-			this->txtmulticador->TabIndex = 3;
+			this->bttniter->Location = System::Drawing::Point(330, 309);
+			this->bttniter->Name = L"bttniter";
+			this->bttniter->Size = System::Drawing::Size(148, 78);
+			this->bttniter->TabIndex = 12;
+			this->bttniter->Text = L"Calcular";
+			this->bttniter->UseVisualStyleBackColor = true;
+			this->bttniter->Click += gcnew System::EventHandler(this, &MyForm::bttniter_Click);
 			// 
-			// bttsumas
+			// txtitecador
 			// 
-			this->bttsumas->Location = System::Drawing::Point(335, 28);
-			this->bttsumas->Name = L"bttsumas";
-			this->bttsumas->Size = System::Drawing::Size(148, 78);
-			this->bttsumas->TabIndex = 4;
-			this->bttsumas->Text = L"Calcular";
-			this->bttsumas->UseVisualStyleBackColor = true;
+			this->txtitecador->Location = System::Drawing::Point(194, 374);
+			this->txtitecador->Name = L"txtitecador";
+			this->txtitecador->Size = System::Drawing::Size(100, 22);
+			this->txtitecador->TabIndex = 11;
 			// 
-			// ressumas
+			// txtitecando
 			// 
-			this->ressumas->AutoSize = true;
-			this->ressumas->Location = System::Drawing::Point(29, 165);
-			this->ressumas->Name = L"ressumas";
-			this->ressumas->Size = System::Drawing::Size(0, 16);
-			this->ressumas->TabIndex = 5;
+			this->txtitecando->Location = System::Drawing::Point(194, 309);
+			this->txtitecando->Name = L"txtitecando";
+			this->txtitecando->Size = System::Drawing::Size(100, 22);
+			this->txtitecando->TabIndex = 10;
+			// 
+			// lblitecador
+			// 
+			this->lblitecador->AutoSize = true;
+			this->lblitecador->Location = System::Drawing::Point(24, 377);
+			this->lblitecador->Name = L"lblitecador";
+			this->lblitecador->Size = System::Drawing::Size(145, 16);
+			this->lblitecador->TabIndex = 9;
+			this->lblitecador->Text = L"Ingrese el multiplicador";
+			// 
+			// lblitecando
+			// 
+			this->lblitecando->AutoSize = true;
+			this->lblitecando->Location = System::Drawing::Point(24, 312);
+			this->lblitecando->Name = L"lblitecando";
+			this->lblitecando->Size = System::Drawing::Size(148, 16);
+			this->lblitecando->TabIndex = 8;
+			this->lblitecando->Text = L"Ingrese el multiplicando";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(544, 313);
+			this->ClientSize = System::Drawing::Size(544, 589);
 			this->Controls->Add(this->tabControl1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
@@ -185,5 +285,53 @@ namespace Tarea1CesarMejia1127922 {
 
 		}
 #pragma endregion
-	};
+	private: System::Void bttsumas_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		try
+		{
+			int multiplicando = Convert::ToInt32(txtmulticando->Text);
+			int multiplicador = Convert::ToInt32(txtmulticador->Text);
+			ressumas->Text = Convert::ToString(recursivosumasmultiplicando(multiplicando, multiplicador, 0));
+		}
+		catch (Exception^ e)
+		{
+			ressumas->Text="Debe ingresar enteros y llenar todos los campos";
+		}
+		
+	}
+		   static int recursivosumasmultiplicando(int a, int b, int contador) {
+			   if (b==0)
+			   {
+				   return contador;
+			   }
+			   else
+			   {
+				   contador += a;
+				   b--;
+				   return(recursivosumasmultiplicando(a, b, contador));
+			   }
+	}
+		   static int iterativosumasmultiplicando(int a, int b) {
+			   int suma = 0;
+			   for (int i = 0; i<b; i++)
+			   {
+				   suma+=a;
+			   }
+			   return suma;
+		   }
+private: System::Void SUMAS_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void bttniter_Click(System::Object^ sender, System::EventArgs^ e) {
+	try
+	{
+		int multiplicando = Convert::ToInt32(txtitecando->Text);
+		int multiplicador = Convert::ToInt32(txtitecador->Text);
+		itersuma->Text = Convert::ToString(iterativosumasmultiplicando(multiplicando, multiplicador));
+	}
+	catch (Exception^ e)
+	{
+		itersuma->Text = "Debe ingresar enteros y llenar todos los campos";
+	}
+}
+};
 }
